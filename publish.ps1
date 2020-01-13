9af10d4d8bd4d66d62e92c0f229be24040d19522
Thomas Bruderer <apophis@apophis.ch>
2020-01-13 15:16:01 +0100

    Publish exe as single file (tested with plugin dependencies) in Release mode

--- publish.ps1
+++ publish.ps1
@@ -1,11 +1,11 @@
-&dotnet publish ProjectGenerator/ProjectGenerator.csproj --configuration Debug --runtime win-x64 --output publish
+&dotnet publish ProjectGenerator/ProjectGenerator.csproj --configuration Release --runtime win-x64 --output publish  /p:PublishSingleFile=true
 
 New-Item -Path "publish" -Name "plugins" -ItemType "directory" -Force
 
 #Plugins
 
-&dotnet publish Plugins/NativeProjects/NativeProjects.csproj --configuration Debug --runtime win-x64 --output "publish\plugins\NativeProjects"
+&dotnet publish Plugins/NativeProjects/NativeProjects.csproj --configuration Release --runtime win-x64 --output "publish\plugins\NativeProjects"
 
-&dotnet publish Plugins/OneCoreProjects/OneCoreProjects.csproj --configuration Debug --runtime win-x64 --output "publish\plugins\OneCoreProjects"
+&dotnet publish Plugins/OneCoreProjects/OneCoreProjects.csproj --configuration Release --runtime win-x64 --output "publish\plugins\OneCoreProjects"
 
-&dotnet publish Plugins/ProjectGeneratorPluginProjects/ProjectGeneratorPluginProjects.csproj --configuration Debug --runtime win-x64 --output "publish\plugins\ProjectGeneratorPluginProjects"
+&dotnet publish Plugins/ProjectGeneratorPluginProjects/ProjectGeneratorPluginProjects.csproj --configuration Release --runtime win-x64 --output "publish\plugins\ProjectGeneratorPluginProjects"
