b5db4088781a420a55bb182843311ef97d5d973a
Thomas Bruderer <apophis@apophis.ch>
2020-01-14 15:12:15 +0100

    Remove Example Plugins

--- publish.ps1
+++ publish.ps1
@@ -4,8 +4,4 @@ New-Item -Path "publish" -Name "plugins" -ItemType "directory" -Force
 
 #Plugins
 
-&dotnet publish Plugins/NativeProjects/NativeProjects.csproj --configuration Release --runtime win-x64 --output "publish\plugins\NativeProjects"
-
-&dotnet publish Plugins/OneCoreProjects/OneCoreProjects.csproj --configuration Release --runtime win-x64 --output "publish\plugins\OneCoreProjects"
-
 &dotnet publish Plugins/ProjectGeneratorPluginProjects/ProjectGeneratorPluginProjects.csproj --configuration Release --runtime win-x64 --output "publish\plugins\ProjectGeneratorPluginProjects"
