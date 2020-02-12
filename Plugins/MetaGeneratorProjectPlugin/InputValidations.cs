79b548a708aaf6f276cf2b1fa8f4e656e671e0b7
Thomas Bruderer <apophis@apophis.ch>
2020-02-12 14:29:37 +0100

    Add Plugin.targets to be able to debug directly with F5 with all plugins in the solution

--- Plugins/MetaGeneratorProjectPlugin/InputValidations.cs
+++ Plugins/MetaGeneratorProjectPlugin/InputValidations.cs
@@ -6,6 +6,6 @@ namespace Messerli.MetaGeneratorProjectPlugin
     internal static class InputValidations
     {
         public static IValidation MetaGeneratorSolutionExists
-            => new SimpleValidation(path => File.Exists(Path.Combine(path, "MetaGenerator.sln")), "No MetaGenerator.sln found at location.");
+            => new SimpleValidation(path => File.Exists(Path.Combine(path, "MetaGenerator.sln")), "No MetaGenerator.sln found at given location.");
     }
 }
