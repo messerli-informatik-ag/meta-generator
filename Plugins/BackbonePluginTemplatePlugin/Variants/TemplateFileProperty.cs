85f58900866b11045af22a1fd21a70050512e72d
Sergio Sutter <sergio.sutter@messerli.ch>
2020-03-25 08:00:53 +0100

    Create template file property

---
+++ Plugins/BackbonePluginTemplatePlugin/Variants/TemplateFileProperty.cs
@@ -0,0 +1,23 @@
+﻿using Messerli.MetaGeneratorAbstractions;
+
+namespace Messerli.BackbonePluginTemplatePlugin.Variants
+{
+    public sealed class TemplateFileProperty
+    {
+        public TemplateFileProperty(
+            IFileGenerator fileGenerator,
+            string repositoryPath,
+            string repositoryName)
+        {
+            FileGenerator = fileGenerator;
+            RepositoryPath = repositoryPath;
+            RepositoryName = repositoryName;
+        }
+
+        public IFileGenerator FileGenerator { get; }
+
+        public string RepositoryPath { get; }
+
+        public string RepositoryName { get; }
+    }
+}
