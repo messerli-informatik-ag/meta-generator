0a877ffa85735e88f969662cbfb6ab25f48fc25e
Sergio Sutter <sergio.sutter@messerli.ch>
2020-03-25 07:52:04 +0100

    Add minimal plugin template

---
+++ Plugins/BackbonePluginTemplatePlugin/Variants/MinimalPluginTemplate/PluginVariant.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Messerli.MetaGeneratorAbstractions;
+
+namespace Messerli.BackbonePluginTemplatePlugin.Variants.MinimalPluginTemplate
+{
+    public class PluginVariant : IPluginVariant
+    {
+        private const string TestFolder = "Test";
+
+        private readonly TemplateFileProperty _templateFileProperty;
+
+        public PluginVariant(TemplateFileProperty templateFileProperty)
+        {
+            _templateFileProperty = templateFileProperty;
+        }
+
+        public List<Task> CreateTemplateFiles()
+            => new List<Task>
+            {
+                _templateFileProperty.FileGenerator.FromTemplate(Template.ProjectFile, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, $"{_templateFileProperty.RepositoryName}.csproj"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.PluginSource, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, "Plugin.cs"), Encoding.UTF8),
+
+                _templateFileProperty.FileGenerator.FromTemplate(Template.TestProjectFile, Path.Combine(_templateFileProperty.RepositoryPath, $"{_templateFileProperty.RepositoryName}.{TestFolder}", $"{_templateFileProperty.RepositoryName}.csproj"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.IntegrationTestSource, Path.Combine(_templateFileProperty.RepositoryPath, $"{_templateFileProperty.RepositoryName}.{TestFolder}", "IntegrationTests.cs"), Encoding.UTF8),
+            };
+    }
+}
---
+++ Plugins/BackbonePluginTemplatePlugin/Variants/MinimalPluginTemplate/Template.cs
@@ -0,0 +1,15 @@
+﻿namespace Messerli.BackbonePluginTemplatePlugin.Variants.MinimalPluginTemplate
+{
+    internal static class Template
+    {
+        public static string ProjectFile => WithPath("Project.csproj.template");
+
+        public static string PluginSource => WithPath("Plugin.Source.template");
+
+        public static string TestProjectFile => WithPath("Project.Test.csproj.template");
+
+        public static string IntegrationTestSource => WithPath("IntegrationTests.Source.template");
+
+        private static string WithPath(string rest) => $"Messerli.BackbonePluginTemplatePlugin.templates.MinimalPluginTemplate.{rest}";
+    }
+}
