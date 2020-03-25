07287c9a07ad3953ca7d2f8ff0655d99fa3f5b4b
Sergio Sutter <sergio.sutter@messerli.ch>
2020-03-25 07:52:51 +0100

    Add plugin template

---
+++ Plugins/BackbonePluginTemplatePlugin/Variants/PluginTemplate/PluginVariant.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messerli.BackbonePluginTemplatePlugin.Variants.PluginTemplate
+{
+    public class PluginVariant : IPluginVariant
+    {
+        private const string GuiFolder = "Gui";
+        private const string ViewFolder = "View";
+        private const string PublicFolder = "Public";
+        private const string IconsFolder = "icons";
+        private const string RegistrarFolder = "Registrar";
+        private const string TemplateFolder = "Templates";
+        private const string TestFolder = "Test";
+
+        private readonly TemplateFileProperty _templateFileProperty;
+
+        public PluginVariant(TemplateFileProperty templateFileProperty)
+        {
+            _templateFileProperty = templateFileProperty;
+        }
+
+        public List<Task> CreateTemplateFiles()
+            => new List<Task>
+            {
+                _templateFileProperty.FileGenerator.FromTemplate(Template.ProjectFile, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, $"{_templateFileProperty.RepositoryName}.csproj"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.Plugin, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, "Plugin.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.Module, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, "Module.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.AssemblyInfo, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, "AssemblyInfo.cs"), Encoding.UTF8),
+
+                _templateFileProperty.FileGenerator.FromTemplate(Template.IPresenter, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, GuiFolder, ViewFolder, "IPresenter.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.Presenter, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, GuiFolder, ViewFolder, "Presenter.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.IView, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, GuiFolder, ViewFolder, "IView.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.View, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, GuiFolder, ViewFolder, "View.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.ResponseRenderer, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, GuiFolder, ViewFolder, "ResponseRenderer.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.ViewModel, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, GuiFolder, ViewFolder, "ViewModel.cs"), Encoding.UTF8),
+
+                _templateFileProperty.FileGenerator.FromTemplate(Template.IController, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, GuiFolder, "IController.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.Controller, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, GuiFolder, "Controller.cs"), Encoding.UTF8),
+
+                _templateFileProperty.FileGenerator.FromTemplate(Template.EndpointConstant, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, RegistrarFolder, "EndpointConstant.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.INavigationRegistrar, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, RegistrarFolder, "INavigationRenderer.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.NavigationRegistrar, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, RegistrarFolder, "NavigationRenderer.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.IRouteRegistrar, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, RegistrarFolder, "IRouteRenderer.cs"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.RouteRegistrar, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, RegistrarFolder, "RouteRegistrar.cs"), Encoding.UTF8),
+
+                _templateFileProperty.FileGenerator.FromTemplate(Template.PluginTemplateMustache, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, TemplateFolder, "PluginTemplate.mustache"), Encoding.UTF8),
+
+                _templateFileProperty.FileGenerator.FromTemplate(Template.Icon, Path.Combine(_templateFileProperty.RepositoryPath, _templateFileProperty.RepositoryName, PublicFolder, IconsFolder, "hello-world.svg"), Encoding.UTF8),
+
+                _templateFileProperty.FileGenerator.FromTemplate(Template.TestProjectFile, Path.Combine(_templateFileProperty.RepositoryPath, $"{_templateFileProperty.RepositoryName}.{TestFolder}", $"{_templateFileProperty.RepositoryName}.csproj"), Encoding.UTF8),
+                _templateFileProperty.FileGenerator.FromTemplate(Template.IntegrationTestSource, Path.Combine(_templateFileProperty.RepositoryPath, $"{_templateFileProperty.RepositoryName}.{TestFolder}", "IntegrationTests.cs"), Encoding.UTF8),
+            };
+    }
+}
---
+++ Plugins/BackbonePluginTemplatePlugin/Variants/PluginTemplate/Template.cs
@@ -0,0 +1,61 @@
+﻿namespace Messerli.BackbonePluginTemplatePlugin.Variants.PluginTemplate
+{
+    internal static class Template
+    {
+        public static string ProjectFile => WithPath("Project.csproj.template");
+
+        public static string Plugin => WithPath("Plugin.Source.template");
+
+        public static string Module => WithPath("Module.Source.template");
+
+        public static string AssemblyInfo => WithPath("AssemblyInfo.Source.template");
+
+        public static string IPresenter => WithSubfolderGuiViewPath("IPresenter.Source.template");
+
+        public static string IView => WithSubfolderGuiViewPath("IView.Source.template");
+
+        public static string Presenter => WithSubfolderGuiViewPath("Presenter.Source.template");
+
+        public static string ResponseRenderer => WithSubfolderGuiViewPath("ResponseRenderer.Source.template");
+
+        public static string View => WithSubfolderGuiViewPath("View.Source.template");
+
+        public static string ViewModel => WithSubfolderGuiViewPath("ViewModel.Source.template");
+
+        public static string Controller => WithSubfolderGuiPath("Controller.Source.template");
+
+        public static string IController => WithSubfolderGuiPath("IController.Source.template");
+
+        public static string Icon => WithSubfolderPublicIconPath("hello-world.Source.template");
+
+        public static string EndpointConstant => WithSubfolderRegistrarPath("EndpointConstant.Source.template");
+
+        public static string INavigationRegistrar => WithSubfolderRegistrarPath("INavigationRegistrar.Source.template");
+
+        public static string IRouteRegistrar => WithSubfolderRegistrarPath("IRouteRegistrar.Source.template");
+
+        public static string NavigationRegistrar => WithSubfolderRegistrarPath("NavigationRegistrar.Source.template");
+
+        public static string RouteRegistrar => WithSubfolderRegistrarPath("RouteRegistrar.Source.template");
+
+        public static string PluginTemplateMustache => WithSubfolderTemplatesPath("PluginTemplate.Source.template");
+
+        public static string TestProjectFile => WithTestPath("Project.Test.csproj.template");
+
+        public static string IntegrationTestSource => WithTestPath("IntegrationTests.Source.template");
+
+        private static string WithPath(string rest) => $"Messerli.BackbonePluginTemplatePlugin.templates.PluginTemplate.{rest}";
+
+        private static string WithSubfolderGuiPath(string rest) => WithPath($"Gui.{rest}");
+
+        private static string WithSubfolderGuiViewPath(string rest) => WithPath($"Gui.View.{rest}");
+
+        private static string WithSubfolderPublicIconPath(string rest) => WithPath($"Public.icons.{rest}");
+
+        private static string WithSubfolderRegistrarPath(string rest) => WithPath($"Registrar.{rest}");
+
+        private static string WithSubfolderTemplatesPath(string rest) => WithPath($"Templates.{rest}");
+
+        private static string WithTestPath(string rest) => WithPath($"Test.{rest}");
+    }
+}
