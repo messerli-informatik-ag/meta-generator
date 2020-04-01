96636cd348c31b4e532a9307a8e2af508e53b799
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-04-01 15:21:03 +0200

    Improve naming

--- FileManipulatorAbstractions/GlobalJsonModificationBuilder.cs
+++ FileManipulatorAbstractions/GlobalJsonModificationBuilder.cs
@@ -1,29 +1,31 @@
 ﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Messerli.FileManipulatorAbstractions
 {
     public sealed class GlobalJsonModificationBuilder
     {
-        private readonly IImmutableList<MsBuildSdk> _sdksToAdd;
+        private readonly IImmutableList<MsBuildSdk> _msBuildSdksToAdd;
 
         public GlobalJsonModificationBuilder()
             : this(ImmutableList<MsBuildSdk>.Empty)
         {
         }
 
-        private GlobalJsonModificationBuilder(IImmutableList<MsBuildSdk> sdksToAdd)
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1305: FieldNamesMustNotUseHungarianNotation", Justification = "Not hungarian notation")]
+        private GlobalJsonModificationBuilder(IImmutableList<MsBuildSdk> msBuildSdksToAdd)
         {
-            _sdksToAdd = sdksToAdd;
+            _msBuildSdksToAdd = msBuildSdksToAdd;
         }
 
         public GlobalJsonModificationBuilder AddMsBuildSdk(MsBuildSdk sdk)
-            => ShallowClone(sdkList: _sdksToAdd.Add(sdk));
+            => ShallowClone(sdksToAdd: _msBuildSdksToAdd.Add(sdk));
 
         public GlobalJsonModification Build()
-            => new GlobalJsonModification(_sdksToAdd);
+            => new GlobalJsonModification(_msBuildSdksToAdd);
 
         private GlobalJsonModificationBuilder ShallowClone(
-            IImmutableList<MsBuildSdk>? sdkList = null)
-            => new GlobalJsonModificationBuilder(sdkList ?? _sdksToAdd);
+            IImmutableList<MsBuildSdk>? sdksToAdd = null)
+            => new GlobalJsonModificationBuilder(sdksToAdd ?? _msBuildSdksToAdd);
     }
 }
