090c8a6a93773b2d0207ff794bc96c2d28b2c200
Jan Nils Ferner <contact@jnferner.com>
2020-04-02 11:17:54 +0200

    Start extracting path retrieving

---
+++ Plugins/BackbonePluginTemplatePlugin/Variants/ITemplatePathRetriever.cs
@@ -0,0 +1,23 @@
+namespace Messerli.BackbonePluginTemplatePlugin.Variants
+{
+    internal interface ITemplatePathRetriever
+    {
+        public string GetPath(string fileName);
+
+        public string GetDatabasePath(string fileName);
+
+        public string GetMigrationsPath(string fileName);
+
+        public string GetGuiPath(string fileName);
+
+        public string GetViewPath(string fileName);
+
+        public string GetIconPath(string fileName);
+
+        public string GetRegistrarPath(string fileName);
+
+        public string GetViewTemplatePath(string fileName);
+
+        public string GetTestPath(string fileName);
+    }
+}
