d8403b66c8c41fdc350cf35b5ca77ef45708040c
Dominic Groth <whitedragon@dragonlabs.ch>
2020-04-03 10:29:41 +0200

    Add more tests

--- FileManipulator.Test/NugetPackageSourcesTest.cs
+++ FileManipulator.Test/NugetPackageSourcesTest.cs
@@ -4,7 +4,6 @@ using Messerli.FileManipulatorAbstractions;
 using Messerli.Test.Utility;
 using Moq;
 using NuGet.Common;
-using NuGet.Configuration;
 using Xunit;
 
 namespace Messerli.FileManipulator.Test
@@ -12,6 +11,8 @@ namespace Messerli.FileManipulator.Test
     public sealed class NugetPackageSourcesTest
     {
         private const string ExamplePackageName = "My Nuget Server";
+        private const string PasswordExamplePackageName = ExamplePackageName;
+
         private static readonly string NewLine = $"{Environment.NewLine}";
 
         private static readonly string MinimalNugetConfig =
@@ -27,12 +28,29 @@ namespace Messerli.FileManipulator.Test
             "  </packageSources>" + $"{NewLine}" +
             "</configuration>" + $"{NewLine}";
 
+        private static readonly string NugetConfigWithDisabledExample =
+            "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + $"{NewLine}" +
+            "<configuration>" + $"{NewLine}" +
+            "  <packageSources>" + $"{NewLine}" +
+            "    <add key=\"My Nuget Server\" value=\"https://nuget.example.ch/v3/index.json\" />" + $"{NewLine}" +
+            "  </packageSources>" + $"{NewLine}" +
+            "  <disabledPackageSources>" + $"{NewLine}" +
+            "    <add key=\"My Nuget Server\" value=\"true\" />" + $"{NewLine}" +
+            "  </disabledPackageSources>" + $"{NewLine}" +
+            "</configuration>" + $"{NewLine}";
+
         private static readonly string NugetConfigWithPasswordExample =
             "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + $"{NewLine}" +
             "<configuration>" + $"{NewLine}" +
             "  <packageSources>" + $"{NewLine}" +
             "    <add key=\"My Nuget Server\" value=\"https://nuget.example.ch/v3/index.json\" />" + $"{NewLine}" +
             "  </packageSources>" + $"{NewLine}" +
+            "  <packageSourceCredentials>" + $"{NewLine}" +
+            "    <My_x0020_Nuget_x0020_Server>" + $"{NewLine}" +
+            "        <add key=\"Username\" value=\"Username\" />" + $"{NewLine}" +
+            "        <add key=\"ClearTextPassword\" value=\"Password\" />" + $"{NewLine}" +
+            "      </My_x0020_Nuget_x0020_Server>" + $"{NewLine}" +
+            "  </packageSourceCredentials>" + $"{NewLine}" +
             "</configuration>" + $"{NewLine}";
 
         private static readonly string NugetConfigWithClearAndNugetOrg =
@@ -64,7 +82,13 @@ namespace Messerli.FileManipulator.Test
             new NugetPackageSource(ExamplePackageName, "https://nuget.example.ch/v3/index.json");
 
         private static readonly NugetPackageSource PasswordExamplePackageSource =
-            new NugetPackageSource(ExamplePackageName, "https://nuget.example.ch/v3/index.json", "Username", "Password");
+            new NugetPackageSource(
+                ExamplePackageName,
+                "https://nuget.example.ch/v3/index.json",
+                "Username",
+                "Password",
+                null,
+                true);
 
         [Theory]
         [MemberData(nameof(AddsPackageSourceData))]
@@ -108,6 +132,36 @@ namespace Messerli.FileManipulator.Test
             => new TheoryData<string, string, string>
             {
                 { NugetConfigWithClearAndNugetOrgAndExample, NugetConfigWithClearAndNugetOrg, ExamplePackageName },
+                { NugetConfigWithPasswordExample, MinimalNugetConfig, PasswordExamplePackageName },
+            };
+
+        [Theory]
+        [MemberData(nameof(EnablePackageSourceData))]
+        public void EnablePackageSource(string configFileContent, string expectedOutput, string packageName)
+            => TestPackageSourceCall(
+                configFileContent,
+                expectedOutput,
+                (packageSources, configFile) => packageSources.Enable(configFile, packageName));
+
+        public static TheoryData<string, string, string> EnablePackageSourceData()
+            => new TheoryData<string, string, string>
+            {
+                { NugetConfigWithDisabledExample, NugetConfigWithExample, ExamplePackageName },
+                { NugetConfigWithExample, NugetConfigWithExample, ExamplePackageName },
+            };
+
+        [Theory]
+        [MemberData(nameof(DisablePackageSourceData))]
+        public void DisablePackageSource(string configFileContent, string expectedOutput, string packageName)
+            => TestPackageSourceCall(
+                configFileContent,
+                expectedOutput,
+                (packageSources, configFile) => packageSources.Disable(configFile, packageName));
+
+        public static TheoryData<string, string, string> DisablePackageSourceData()
+            => new TheoryData<string, string, string>
+            {
+                { NugetConfigWithExample, NugetConfigWithDisabledExample, ExamplePackageName },
             };
 
         private void TestPackageSourceCall(
@@ -124,16 +178,16 @@ namespace Messerli.FileManipulator.Test
             Assert.Equal(expectedFileContent, ReadNugetTestFile(testEnvironment));
         }
 
-        private static INugetPackageSources CreateNugetPackageSources()
-            => new NugetPackageSources(() => new Mock<ILogger>().Object);
+        private static INugetPackageSources CreateNugetPackageSources(ILogger? logger = null)
+            => new NugetPackageSources(() => logger ?? new Mock<ILogger>().Object);
 
-        private string GetNugetFilePath(TestEnvironmentProvider testEnvironmentProvider)
+        private static string GetNugetFilePath(TestEnvironmentProvider testEnvironmentProvider)
             => Path.Combine(testEnvironmentProvider.RootDirectory, "nuget.config");
 
-        private void CreateNugetTestFile(TestEnvironmentProvider testEnvironmentProvider, string fileContent)
+        private static void CreateNugetTestFile(TestEnvironmentProvider testEnvironmentProvider, string fileContent)
             => File.WriteAllText(GetNugetFilePath(testEnvironmentProvider), fileContent);
 
-        private string ReadNugetTestFile(TestEnvironmentProvider testEnvironmentProvider)
+        private static string ReadNugetTestFile(TestEnvironmentProvider testEnvironmentProvider)
             => File.ReadAllText(GetNugetFilePath(testEnvironmentProvider));
     }
 }
