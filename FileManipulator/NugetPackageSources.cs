858d0cdab1c5f8584f0c7fb420c7261e24b317ef
Dominic Groth <whitedragon@dragonlabs.ch>
2020-04-03 08:20:20 +0200

    Add nuget package sources implementation

---
+++ FileManipulator/NugetPackageSources.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Messerli.FileManipulatorAbstractions;
+
+namespace Messerli.FileManipulator
+{
+    public sealed class NugetPackageSources : INugetPackageSources
+    {
+        public void List(string configFile, string format)
+            => throw new NotImplementedException();
+
+        public void Add(string configFile, NugetPackageSource packageSource)
+            => throw new NotImplementedException();
+
+        public void Update(string configFile, NugetPackageSource packageSource)
+            => throw new NotImplementedException();
+
+        public void Remove(string configFile, string name)
+            => throw new NotImplementedException();
+
+        public void Enable(string configFile, string name)
+            => throw new NotImplementedException();
+
+        public void Disable(string configFile, string name)
+            => throw new NotImplementedException();
+    }
+}
