7cd12102c82451b9add6f90c99e8a6a293ef2dd8
Dominic Groth <whitedragon@dragonlabs.ch>
2020-04-03 10:32:45 +0200

    Implement nuget config sources

--- FileManipulator/NugetPackageSources.cs
+++ FileManipulator/NugetPackageSources.cs
@@ -1,26 +1,101 @@
 ﻿using System;
+using System.IO;
 using Messerli.FileManipulatorAbstractions;
+using NuGet.Commands;
+using NuGet.Common;
 
 namespace Messerli.FileManipulator
 {
+    public delegate ILogger GetLogger();
+
     public sealed class NugetPackageSources : INugetPackageSources
     {
-        public void List(string configFile, string format)
-            => throw new NotImplementedException();
+        private const string MinimalNugetConfigContent =
+            "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+            "<configuration>\n" +
+            "</configuration>\n";
+
+        private readonly Func<ILogger> _getLogger;
+
+        public NugetPackageSources(GetLogger getLogger)
+        {
+            _getLogger = () => getLogger();
+        }
 
         public void Add(string configFile, NugetPackageSource packageSource)
-            => throw new NotImplementedException();
+        {
+            var addSourceArgs = new AddSourceArgs
+            {
+                Configfile = configFile,
+
+                Name = packageSource.Name,
+                Password = packageSource.Password,
+                Source = packageSource.Source,
+                Username = packageSource.Username,
+                ValidAuthenticationTypes = packageSource.ValidAuthenticationTypes,
+                StorePasswordInClearText = packageSource.StorePasswordInClearText,
+            };
+
+            CreateMinimalConfigIfNotExistOrIsEmpty(configFile);
+            AddSourceRunner.Run(addSourceArgs, _getLogger);
+        }
 
         public void Update(string configFile, NugetPackageSource packageSource)
-            => throw new NotImplementedException();
+        {
+            var updateSourceArgs = new UpdateSourceArgs
+            {
+                Configfile = configFile,
+
+                Name = packageSource.Name,
+                Password = packageSource.Password,
+                Source = packageSource.Source,
+                Username = packageSource.Username,
+                ValidAuthenticationTypes = packageSource.ValidAuthenticationTypes,
+                StorePasswordInClearText = packageSource.StorePasswordInClearText,
+            };
+
+            UpdateSourceRunner.Run(updateSourceArgs, _getLogger);
+        }
 
         public void Remove(string configFile, string name)
-            => throw new NotImplementedException();
+        {
+            var removeSourceArgs = new RemoveSourceArgs
+            {
+                Configfile = configFile,
+                Name = name,
+            };
+
+            RemoveSourceRunner.Run(removeSourceArgs, _getLogger);
+        }
 
         public void Enable(string configFile, string name)
-            => throw new NotImplementedException();
+        {
+            var enableSourceArgs = new EnableSourceArgs
+            {
+                Configfile = configFile,
+                Name = name,
+            };
+
+            EnableSourceRunner.Run(enableSourceArgs, _getLogger);
+        }
 
         public void Disable(string configFile, string name)
-            => throw new NotImplementedException();
+        {
+            var disableSourceArgs = new DisableSourceArgs
+            {
+                Configfile = configFile,
+                Name = name,
+            };
+
+            DisableSourceRunner.Run(disableSourceArgs, _getLogger);
+        }
+
+        private static void CreateMinimalConfigIfNotExistOrIsEmpty(string configFile)
+        {
+            if (!File.Exists(configFile) || string.IsNullOrWhiteSpace(File.ReadAllText(configFile)))
+            {
+                File.WriteAllText(configFile, MinimalNugetConfigContent);
+            }
+        }
     }
 }
