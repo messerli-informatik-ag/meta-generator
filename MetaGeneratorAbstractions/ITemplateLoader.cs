6470a7ba614a3a29bf46455c71102066795b0c79
Jan Nils Ferner <contact@jnferner.com>
2020-04-03 10:06:02 +0200

    Document glob feature

--- MetaGeneratorAbstractions/ITemplateLoader.cs
+++ MetaGeneratorAbstractions/ITemplateLoader.cs
@@ -7,6 +7,11 @@ namespace Messerli.MetaGeneratorAbstractions
     {
         string GetTemplate(string templateName);
 
+        /// <summary>
+        /// Embedded resources need to annotated with LogicalName="%(Identity)" in order to
+        /// preserve the directory information needed for this method.
+        /// <param name="glob">The glob pattern matching the embedded resources used as templates</param>
+        /// </summary>
         IEnumerable<Template> GetTemplatesFromGlob(string glob);
 
         Stream? GetTemplateStream(string templateName);
