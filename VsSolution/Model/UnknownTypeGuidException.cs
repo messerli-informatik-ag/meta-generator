23589c206e54b64a31d7425210bf1dd514e14b56
Thomas Bruderer <apophis@apophis.ch>
2020-04-15 14:22:01 +0200

    Fixes #40 added unknown project type * Added new project types * Generate a useful exception when an unknown type is detected

---
+++ VsSolution/Model/UnknownTypeGuidException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Messerli.VsSolution.Model
+{
+    [Serializable]
+    internal class UnknownTypeGuidException : Exception
+    {
+        public UnknownTypeGuidException()
+        {
+        }
+
+        public UnknownTypeGuidException(Guid guid)
+            : this($"Unknown type guid in solution found: {guid}")
+        {
+        }
+
+        public UnknownTypeGuidException(string message)
+            : base(message)
+        {
+        }
+
+        public UnknownTypeGuidException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected UnknownTypeGuidException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
