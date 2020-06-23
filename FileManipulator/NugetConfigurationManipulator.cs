d56d46c51e2aedbfc1a9755dc956141b46305fc1
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-06-23 09:03:09 +0200

    Remove unused using declarations

--- FileManipulator/NugetConfigurationManipulator.cs
+++ FileManipulator/NugetConfigurationManipulator.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Messerli.FileManipulatorAbstractions;
-using NuGet.DependencyResolver;
 
 namespace Messerli.FileManipulator
 {
