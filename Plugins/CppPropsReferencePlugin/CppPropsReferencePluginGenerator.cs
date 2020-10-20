10317209dc1518a0c97bc09c3fb24258a14c604c
Sergio Sutter <sergio.sutter@messerli.ch>
2020-10-20 08:51:06 +0200

    Create a new cpp props reference plugin for this generator

---
+++ Plugins/CppPropsReferencePlugin/CppPropsReferencePluginGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Messerli.CommandLineAbstractions;
+using Messerli.MetaGeneratorAbstractions;
+using Messerli.MetaGeneratorAbstractions.UserInput;
+
+namespace Messerli.CppPropsReferencePlugin
+{
+    public class CppPropsReferencePluginGenerator : IMetaGenerator
+    {
+        private const string VariableDeclarations = "Messerli.CppPropsReferencePlugin.templates.VariableDeclarations.json";
+        private const string GeneratorName = "GeneratorName";
+
+        private readonly IConsoleWriter _consoleWriter;
+        private readonly IFileGenerator _fileGenerator;
+        private readonly IUserInputProvider _userInputProvider;
+        private readonly IVariableProvider _variableProvider;
+
+        public CppPropsReferencePluginGenerator(IConsoleWriter consoleWriter, IFileGenerator fileGenerator, IUserInputProvider userInputProvider, IVariableProvider variableProvider)
+        {
+            _consoleWriter = consoleWriter;
+            _fileGenerator = fileGenerator;
+            _userInputProvider = userInputProvider;
+            _variableProvider = variableProvider;
+        }
+
+        public string Description => "Adds C++ project references to properties";
+
+        public string Name => "cpp-props-reference-plugin";
+
+        public void Register()
+        {
+            _userInputProvider.RegisterVariablesFromTemplate(VariableDeclarations);
+        }
+
+        public void Prepare()
+        {
+            // for example tfs checkout
+        }
+
+        public void Generate()
+        {
+            var generatorName = _userInputProvider.Value(GeneratorName);
+
+            _consoleWriter.WriteLine($"Creating ... '{generatorName}' ...");
+
+            var tasks = new List<Task>
+            {
+            };
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        public void TearDown()
+        {
+            // for example git add
+        }
+    }
+}
---
+++ Plugins/CppPropsReferencePlugin/CppPropsReferencePluginModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Messerli.MetaGeneratorAbstractions;
+
+namespace Messerli.CppPropsReferencePlugin
+{
+    public class CppPropsReferencePluginModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CppPropsReferencePluginGenerator>().As<IMetaGenerator>().InstancePerLifetimeScope();
+        }
+    }
+}
